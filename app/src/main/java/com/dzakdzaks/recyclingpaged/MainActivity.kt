7ba561dd83da8c70ac541f1dec813f2ebe3ad859
dzakdzaks <dzakyvijar1>
2020-02-26 19:04:08 +0700

    magabut

--- app/src/main/java/com/dzakdzaks/recyclingpaged/MainActivity.kt
+++ app/src/main/java/com/dzakdzaks/recyclingpaged/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.dzakdzaks.recyclingpaged
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.ColorUtils
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -20,7 +26,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        initBottomBar()
+        initRecycler(viewModel)
+    }
 
+    private fun initRecycler(viewModel: MyViewModel) {
         main_recycler_view.setupAdapterPaged<NowPlayingMovie>(R.layout.item_movie) { adapter, context, list ->
 
             bind { itemView, position, item ->
@@ -60,6 +70,23 @@ class MainActivity : AppCompatActivity() {
                 submitNetwork(it)
             })
 
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun initBottomBar() {
+        color.setBackgroundColor(ColorUtils.setAlphaComponent(Color.parseColor("#FF8888"), 60))
+
+        expandable_bottom_bar.onItemSelectedListener = { v, item ->
+            val anim = ViewAnimationUtils.createCircularReveal(
+                color,
+                expandable_bottom_bar.x.toInt() + v.x.toInt() + v.width / 2,
+                expandable_bottom_bar.y.toInt() + v.y.toInt() + v.height / 2, 0F,
+                findViewById<View>(android.R.id.content).height.toFloat()
+            )
+            color.setBackgroundColor(ColorUtils.setAlphaComponent(item.activeColor, 60))
+            anim.duration = 420
+            anim.start()
         }
     }
 }
